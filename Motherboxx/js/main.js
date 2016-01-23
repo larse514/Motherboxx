246f6018bdfad5bd092b7b1e52429db019b232e6
larse514 <andrew.larse514@gmail.com>
2016-01-22 18:00:46 -0600

    adding angular reference

--- Motherboxx/js/main.js
+++ Motherboxx/js/main.js
@@ -1,5 +1,5 @@
 var app = {
-	
+
 	showAlert: function (message, title) {
 		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
@@ -13,14 +13,21 @@ var app = {
 	},
 	helloworld: function(){
 		$.ajax({
-			url:'http://localhost:8080/helloworld',
-			dataType: 'jsonp',
+			url:'http://mbloginservice-larslarslars.rhcloud.com/login',
+			contentType:"application/json",
+			type: "POST",
+			data:{
+			},
 			success: function(result) {
-				alert(result);
-			}  // edited
+				alert(result.message);
+			},
+			error: function(err){
+				console.log(err)
+			}// edited
 		});
 	}
-
 };
+
+
 
 app.initialize();

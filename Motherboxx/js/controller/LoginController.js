036af94ee790e24be8061fab8e1bb2d3e7ed5c1d
larse514 <andrew.larse514@gmail.com>
2016-01-23 08:53:23 -0600

    testing error message

--- Motherboxx/js/controller/LoginController.js
+++ Motherboxx/js/controller/LoginController.js
@@ -6,11 +6,16 @@
     var angularApp = angular.module("loginViewer", []);
     var LoginController = function ($scope, $http) {
 
-        $http.get("http://mbloginservice-larslarslars.rhcloud.com/test")
-            .then(function (response) {
+        $http.post(
+            "http://mblogins0ervice-larslarslars.rhcloud.com/login",
+            {
+                userName: "test", password: "password"
+            }
+        ).then(function (response) {
             $scope.dbinfo = response.data;
+        }, function(error){
+            app.showAlert("Failed to load server data","Server Error")
         })
-
     };
 
     angularApp.controller("LoginController", LoginController)
